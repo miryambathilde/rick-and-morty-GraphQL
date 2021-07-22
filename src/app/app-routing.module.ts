d15fa8c1d7e6d1834b11221d39e94c65ea65cc79
Miryam Bathilde <mbathilde@gmail.com>
2021-07-22 19:59:59 +0200

    changed routes and add redirect to

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,53 @@
+/* eslint-disable max-len */
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) }, { path: 'episodes', loadChildren: () => import('./components/pages/episodes/episodes.module').then(m => m.EpisodesModule) }, { path: 'not', loadChildren: () => import('./components/pages/notFound/not-found/not-found.module').then(m => m.NotFoundModule) }, { path: 'character-list', loadChildren: () => import('./components/pages/characters/characters-list/characters-list.module').then(m => m.CharactersListModule) }, { path: 'character-details', loadChildren: () => import('./components/pages/characters/characters-details/characters-details.module').then(m => m.CharactersDetailsModule) }, { path: 'about', loadChildren: () => import('./components/pages/about/about/about.module').then(m => m.AboutModule) }];
+const routes: Routes = [
+  { path: '', redirectTo: 'characters', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./components/pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'episodes',
+    loadChildren: () =>
+      import('./components/pages/episodes/episodes.module').then(
+        m => m.EpisodesModule
+      )
+  },
+  {
+    path: 'character-list',
+    loadChildren: () =>
+      import(
+        './components/pages/characters/characters-list/characters-list.module'
+      ).then(m => m.CharactersListModule)
+  },
+  {
+    path: 'character-details/:id',
+    loadChildren: () =>
+      import(
+        './components/pages/characters/characters-details/characters-details.module'
+      ).then(m => m.CharactersDetailsModule)
+  },
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('./components/pages/about/about/about.module').then(
+        m => m.AboutModule
+      )
+  },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./components/pages/notFound/not-found/not-found.module').then(
+        m => m.NotFoundModule
+      )
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
