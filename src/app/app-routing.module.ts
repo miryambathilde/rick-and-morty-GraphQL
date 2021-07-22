e41c6d47968f0dcd26b1d4622e73b1969a5a0b29
Miryam Bathilde <mbathilde@gmail.com>
2021-07-22 20:04:04 +0200

    corrections in app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'characters', pathMatch: 'full' },
+  { path: '', redirectTo: 'character-list', pathMatch: 'full' },
   {
     path: 'home',
     loadChildren: () =>
