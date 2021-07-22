abbcd03cd62bc1406162095b9f74757c912c5578
Miryam Bathilde <mbathilde@gmail.com>
2021-07-22 19:21:57 +0200

    module components eposides and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) }];
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) }, { path: 'episodes', loadChildren: () => import('./components/pages/episodes/episodes.module').then(m => m.EpisodesModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
