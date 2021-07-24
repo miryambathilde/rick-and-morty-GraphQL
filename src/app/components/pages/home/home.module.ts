36af540d796f0e4880f1c5dd8a02c060bc7e4e0c
Miryam Bathilde <mbathilde@gmail.com>
2021-07-24 21:51:41 +0200

    rendering in home page favorites characters

--- src/app/components/pages/home/home.module.ts
+++ src/app/components/pages/home/home.module.ts
@@ -3,13 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
+import { CharactersCardModule } from '../characters/characters-card/characters-card.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  imports: [CommonModule, HomeRoutingModule, CharactersCardModule]
 })
-export class HomeModule { }
+export class HomeModule {}
