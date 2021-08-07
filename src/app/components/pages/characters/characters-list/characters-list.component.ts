8cd04738e86c90f741c07122623ebd4fb81c40b8
Miryam <mbathilde@gmail.com>
2021-08-07 14:50:23 +0200

    scroll infinite implemented

--- src/app/components/pages/characters/characters-list/characters-list.component.ts
+++ src/app/components/pages/characters/characters-list/characters-list.component.ts
@@ -1,23 +1,50 @@
-import { Component } from '@angular/core';
+/* eslint-disable max-len */
+import { DOCUMENT } from '@angular/common';
+import { Component, HostListener, Inject } from '@angular/core';
 import { DataService } from '@app/shared/services/data.service';
-import { LocalStorageService } from '@app/shared/services/localStorage.service';
 
 @Component({
   selector: 'app-characters-list',
   template: `
-    <section class="character__list">
+    <section class="character__list" infiniteScroll (scrolled)="onScrollDown()">
       <app-characters-card
         *ngFor="let character of characters$ | async"
         [character]="character"
       ></app-characters-card>
+      <button *ngIf="showButton" (click)="onScrollTop()" class="button">
+        ⬆
+      </button>
     </section>
   `,
   styleUrls: ['./characters-list.component.scss']
 })
 export class CharactersListComponent {
   characters$ = this.dataSvc.characters$;
+  showButton = false;
+
+  private scrollHeight = 500;
+  private pageNum = 1;
+
   constructor(
-    private dataSvc: DataService,
-    private localStorageSvc: LocalStorageService
+    @Inject(DOCUMENT) private document: Document,
+    private dataSvc: DataService
   ) {}
+
+  @HostListener('window:scroll')
+  onWindowScroll(): void {
+    const yOffset = window.pageYOffset;
+    const scrollTop = this.document.documentElement.scrollTop;
+    // esto es para que el boton solo se muestre cuando haya recorrido 500px verticalmente y le aplicamos la directiva ngIf en el botton con esta propiedad //
+    this.showButton = (yOffset || scrollTop) > this.scrollHeight;
+  }
+
+  onScrollTop(): void {
+    this.document.documentElement.scrollTop = 0;
+  }
+
+  onScrollDown(): void {
+    //cada vez que se haga scroll hacia abajo se ejecuta
+    this.pageNum++;
+    this.dataSvc.getCharactersByPage(this.pageNum);
+  }
 }
--- src/app/components/pages/characters/characters-list/characters-list.module.ts
+++ src/app/components/pages/characters/characters-list/characters-list.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { CharactersListRoutingModule } from './characters-list-routing.module';
 import { CharactersListComponent } from './characters-list.component';
 import { CharactersCardModule } from '@characters/characters-card/characters-card.module';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 @NgModule({
   declarations: [CharactersListComponent],
-  imports: [CommonModule, CharactersListRoutingModule, CharactersCardModule]
+  imports: [
+    CommonModule,
+    CharactersListRoutingModule,
+    CharactersCardModule,
+    InfiniteScrollModule
+  ]
 })
 export class CharactersListModule {}
